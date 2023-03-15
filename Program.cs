90cbf31ae2f3dff2b2b96d985f9943e901e710bf
Gabriele Pagnotta <gabriele.pagnotta1998@gmail.com>
2023-03-15 16:35:35 +0100

    fine esercizio base

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] cibi = { "lasagne al ragu", "pasta al pesto", "gnocchi", "pizza margherita", "spaghetti al pomodoro" };
+
+//La lunghezza della classifica:
+
+var lunghezza = cibi.Length;
+
+var ciao = "la lunghezza è" + " " +  lunghezza;
+
+Console.WriteLine(ciao);
+
+
+//La vostra classifica (dunque stampare l’intero array in ordine indicando la posizione in classifica):
+
+for (int i = 0; i < cibi.Length; i++)
+{
+    
+    Console.WriteLine(i+1 + " " + cibi[i]);
+}
+
+
+//Il vostro cibo top (prima posizione della classifica):
+
+Console.WriteLine(cibi[0]);
+
+
+//Il vostro cibo preferito ma non troppo (ultima posizione della classifica):
+
+Console.WriteLine(cibi[4]);
